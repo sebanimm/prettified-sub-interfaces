7f14c3891b10ee9615e22fdc9f76f6aaf4f3b978
sebanimm <gks32114221@gmail.com>
2024-05-27 14:35:59 +0900

    Add hover feature

--- src/extension.ts
+++ src/extension.ts
@@ -1,32 +1,27 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const languages = ["typescript", "typescriptreact"] as const;
+
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "prettified-sub-interfaces" is now active!',
-  );
+  console.log("activated");
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "prettified-sub-interfaces.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage(
-        "Hello World from prettified-sub-interfaces!",
-      );
+  const hoverProvider: vscode.HoverProvider = {
+    provideHover(document, position, token) {
+      console.log("triggered");
+      return {
+        contents: ["hover content"],
+      };
     },
+  };
+
+  context.subscriptions.push(
+    ...languages.map((language) =>
+      vscode.languages.registerHoverProvider(
+        {
+          language,
+        },
+        hoverProvider,
+      ),
+    ),
   );
-
-  context.subscriptions.push(disposable);
 }
-
-// This method is called when your extension is deactivated
-export function deactivate() {}
