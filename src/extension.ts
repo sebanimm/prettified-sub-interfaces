e63258c1b443430b13135742e40099ff81be1cef
sebanimm <gks32114221@gmail.com>
2024-05-30 17:30:35 +0900

    Make hoverProvider shows text only at sub interfaces

--- src/extension.ts
+++ src/extension.ts
@@ -4,11 +4,28 @@ export function activate(context: vscode.ExtensionContext) {
   const registeredUris = new Set<string>();
 
   const hoverProvider: vscode.HoverProvider = {
-    provideHover(document, position, token) {
-      console.log("triggered");
-      return {
-        contents: ["hover content"],
-      };
+    provideHover(document, position) {
+      const range = document.getWordRangeAtPosition(position);
+
+      if (!range) {
+        return null;
+      }
+
+      const word = document.getText(range);
+      const interfaceRegex = /(?:\binterface\s+(\w+))\s*(?:extends\s+(\w+))/g;
+      const text = document.getText();
+
+      let match;
+
+      while ((match = interfaceRegex.exec(text)) !== null) {
+        if (match[1] === word) {
+          return new vscode.Hover(
+            `This is interface '${match[1]}' declaration.`,
+          );
+        }
+      }
+
+      return null;
     },
   };
 
@@ -31,11 +48,11 @@ export function activate(context: vscode.ExtensionContext) {
       registeredUris.add(key);
 
       context.subscriptions.push(
-      vscode.languages.registerHoverProvider(
-        { language: languageId },
-        hoverProvider,
-      ),
-    );
+        vscode.languages.registerHoverProvider(
+          { language: languageId },
+          hoverProvider,
+        ),
+      );
     }),
   );
 }
